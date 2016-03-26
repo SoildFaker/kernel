9ac801b9afda3379fbc20bbf95cae9f34edf6e61
soildfaker <enbinli@outlook.com>
2016-03-26 19:34:19 +0800

    boot loader sources

--- boot/boot.s
+++ boot/boot.s
@@ -1,29 +1,37 @@
-! boot.s startup file
+.globl begtext, begdata, begbss, endtext, enddata, endbss
+.text
+begtext:
+.data
+begdata:
+.bss
+begbss:
+.text
+BOOTSEG = 0x07c0
 
-BOOTSEG	equ	0x07c0
-
-[BITS 32]
-
-section .init.text
-
-[GLOBAL	start]
-
+entry start
 start:
-		jmpi	go,BOOTSEG
-go:		mov		ax,cs
-		mov		ds,ax
-		mov		es,ax
-		mov		[msg1+17],ah
-		mov		cx,#20
-		mov		dx,#0x1004
-		mov		bx,#0x000c
-		mov		bp,#msg1
-		mov		ax,#0x1301
-		int		0x10
-loop0:	jmp		loop0
-msg1:	.ascii	"loading system ..."
-		.byte	13,10
+	jmpi go,BOOTSEG
+go:	mov	ax,cs
+	mov ds,ax
+	mov es,ax
+	mov [msg1+17],ah
+	mov cx,#20
+	mov dx,#0x1004
+	mov bx,#0x000c
+	mov bp,#msg1
+	mov ax,#0x1301
+	int 0x10
+loop0:	jmp loop0
+msg1:	.ascii "Loading System..."
+		.byte 13,10
 
-.org	510
-		.word	0xAA55
+.org 510
+	  .word 0xAA55
+
+	  .text
+	  endtext:
+	  .data
+	  enddata:
+	  .bss
+	  endbss:
 
