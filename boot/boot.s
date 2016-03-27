f485ea87f5ce4bafc9b0cc7830ec226527fd9d32
soildfaker <enbinli@outlook.com>
2016-03-27 11:29:03 +0800

    boot start

--- boot/boot.s
+++ boot/boot.s
@@ -1,4 +1,5 @@
-.globl begtext, begdata, begbss, endtext, enddata, endbss
+
+.globl	begtext, begdata, begbss, endtext, enddata, endbss
 .text
 begtext:
 .data
@@ -6,32 +7,73 @@ begdata:
 .bss
 begbss:
 .text
+
 BOOTSEG = 0x07c0
+SYSSEG = 0x1000
+SYSLEN = 17
 
 entry start
 start:
-	jmpi go,BOOTSEG
-go:	mov	ax,cs
-	mov ds,ax
-	mov es,ax
-	mov [msg1+17],ah
-	mov cx,#20
-	mov dx,#0x1004
-	mov bx,#0x000c
-	mov bp,#msg1
-	mov ax,#0x1301
-	int 0x10
-loop0:	jmp loop0
-msg1:	.ascii "Loading System..."
-		.byte 13,10
+	jmpi	go,BOOTSEG
+go:	mov		ax,cs
+	mov		ds,ax
+	mov		ss,ax
+	mov		sp,#0x400
+
+load_system:
+	mov		dx,#0x0000
+	mov		cx,#0x0002
+	mov		ax,#SYSSEG
+	mov		es,ax
+	xor		bx,bx
+	mov		ax,#0x200+SYSLEN
+	int		0x13
+	jnc		ok_load
+die:	jmp		die
+
+ok_load:
+	cli
+	mov		ax,#SYSSEG
+	mov		ds,ax
+	xor		ax,ax
+	mov		es,ax
+	mov		cx,#0x1000
+	sub		si,si
+	sub		di,di
+	rep
+	movw
+
+	mov		ax,#BOOTSEG
+	mov		ds,ax
+	lidt	idt_48
+	lgdt	gdt_48
+
+	mov		ax,#0x0001
+	lmsw	ax
+	jmpi	0,8
+
+gdt:	.word	0,0,0,0
+	.word	0x07FF
+	.word	0x0000
+	.word	0x9A00
+	.word	0x00C0
+
+	.word	0x07FF
+	.word	0x0000
+	.word	0x9A00
+	.word	0x00C0
+
+idt_48:		.word	0
+			.word	0,0
+
+gdt_48:		.word	0x7ff
+			.word	0x7c00+gdt,0
 
 .org 510
 	  .word 0xAA55
-
-	  .text
-	  endtext:
-	  .data
-	  enddata:
-	  .bss
-	  endbss:
-
+.text
+endtext:
+.data
+enddata:
+.bss
+endbss:
