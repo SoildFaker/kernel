e1111b858c2fddf9445da7b20a0e1382993e4831
soildfaker <enbinli@outlook.com>
2016-04-03 13:21:20 +0800

    portect mode test1

--- boot/boot.s
+++ boot/boot.s
@@ -6,15 +6,43 @@ org		0100h
 ; GDT
 ;			
 LABEL_GDT:			Descriptor	0,			0,				0
+LABEL_DESC_NORMAL:	Descriptor	0,			0ffffh,			DA_DRW
+LABEL_DESC_CODE16:	Descriptor	0,			0ffffh,			DA_C
 LABEL_DESC_CODE32:	Descriptor	0,			Seg32Len -1,	DA_C + DA_32
+LABEL_DSEC_DATA:	Descriptor	0,			DataLen - 1,	DA_DRW
+LABEL_DESC_STACK:	Descriptor	0,			TopOfStack,		DA_DRWA + DA_32
+LABEL_DESC_TEST:	Descriptor	0500000h,	0ffffh,			DA_DRW
 LABEL_DESC_VIDEO:	Descriptor	0B8000h,	0FFFFh,			DA_DRW
 
 GdtLen		equ		$ - LABEL_GDT
 GdtPtr		dw		GdtLen - 1
 			dd		0
 
+SelectorNormal		equ		LABEL_DESC_NORMAL - LABEL_GDT
+SelectorCode16		equ		LABEL_DESC_CODE16 - LABEL_GDT
+SelectorData		equ		LABEL_DSEC_DATA - LABEL_GDT
+SelectorStack		equ		LABEL_DESC_STACK - LABEL_GDT
+SelectorTest		equ		LABEL_DESC_TEST - LABEL_GDT
 SelectorCode32		equ		LABEL_DESC_CODE32 - LABEL_GDT
 SelectorVideo		equ		LABEL_DESC_VIDEO - LABEL_GDT
+
+[SECTION .data1]
+ALIGN	32
+[BITS 32]
+LABEL_DATA:
+SPValueInRealMode		dw		0
+PMMessage:				db		"In Protect Mode Now ...", 0
+OffsetPMMessage			equ		PMMessage - $$
+StrTest:				db		"ABCDEFGHIJKLMNOPQRSTUVWXYZ", 0
+OffsetStrTest			equ		StrTest - $$
+DataLen					equ		$ - LABEL_DATA
+
+[SECTION .gs]
+ALIGN	32
+[BITS 32]
+LABEL_STACK:
+	  times		512		db	0
+TopOfStack:	 equ	$ - LABEL_STACK - 1
 
 [SECTION .s16]
 [BITS 16]
@@ -25,6 +53,8 @@ LABEL_BEGIN:
 	  mov	ss,ax
 	  mov	sp,0100h
 
+	  mov	[LABLE_GO_BACK_TO_REAL + 3],ax
+
 	  xor	eax,eax
 	  mov	ax,cs
 	  shl	eax,4
@@ -33,6 +63,35 @@ LABEL_BEGIN:
 	  shr	eax,16
 	  mov	byte [LABEL_DESC_CODE32 + 4],al
 	  mov	byte [LABEL_DESC_CODE32 + 7],ah
+
+	  xor	eax,eax
+	  mov	ax,ds
+	  shl	eax,4
+	  add	eax,LABEL_DATA
+	  mov	word [LABEL_DSEC_DATA + 2],ax
+	  shr	eax,16
+	  mov	byte [LABEL_DSEC_DATA + 4],al
+	  mov	byte [LABEL_DSEC_DATA + 7],ah
+
+	  xor	eax,eax
+	  mov	ax,ss
+	  shl	eax,4
+	  add	eax,LABEL_STACK
+	  mov	word [LABEL_DESC_STACK + 2],ax
+	  shr	eax,16
+	  mov	byte [LABEL_DESC_STACK + 4],al
+	  mov	byte [LABEL_DESC_STACK + 7],ah
+
+	  xor	eax,eax
+	  mov	ax,cs
+	  shl	eax,4
+	  add	eax,LABEL_SEG_CODE16
+	  mov	word [LABEL_DESC_CODE16 + 2],ax
+	  shr	eax,16
+	  mov	byte [LABEL_DESC_CODE16 + 4],al
+	  mov	byte [LABEL_DESC_CODE16 + 7],ah
+
+
 
 	  xor	eax,eax
 	  mov	ax,ds
@@ -53,19 +112,156 @@ LABEL_BEGIN:
 	  mov	cr0,eax
 
 	  jmp	dword SelectorCode32:0
+LABEL_REAL_ENTRY:
+	  mov	ax,cs
+	  mov	ds,ax
+	  mov	es,ax
+	  mov	ss,ax
+
+	  mov	sp,[SPValueInRealMode]
+
+	  in	al,92h
+	  and	al,11111101b
+	  out	92h,al
+
+	  sti
+
+	  mov	ax,4c00h
+	  int	21h
 [SECTION .s32]
 [BITS 32]
 
 LABEL_SEG_CODE32:
+	  mov	ax,SelectorData
+	  mov	ds,ax
+	  mov	ax,SelectorTest
+	  mov	es,ax
 	  mov	ax,SelectorVideo
 	  mov	gs,ax
 
-	  mov	edi,(80 * 11 +79) * 2
+	  mov	ax,SelectorStack
+	  mov	ss,ax
+
+	  mov	esp,TopOfStack
 
 	  mov	ah,0ch
-	  mov	al,'P'
+	  xor	edi,edi
+	  xor	esi,esi
+	  mov	esi,OffsetPMMessage
+	  mov	edi,(80 * 11 +79) * 2
+	  cld
+
+.1:
+	  lodsb 
+	  test	al,al
+	  jz	.2
 	  mov	[gs:edi],ax
+	  add	edi,2
+	  jmp	.1
 
-	  jmp	$
+.2:
+	  call	DispReturn
 
+	  call	TestRead
+	  call	TestWrite
+	  call	TestRead
+
+	  jmp	SelectorCode16:0
+
+TestRead:
+	  xor	esi,esi
+	  mov	ecx,8
+
+.loop:
+	  mov	al,[es:esi]
+	  call	DispAll
+	  inc	esi
+	  loop	.loop
+	  call DispReturn
+	  ret
+
+TestWrite:
+	  push	esi
+	  push	edi
+	  xor	esi,esi
+	  xor	edi,edi
+	  mov	esi,OffsetStrTest
+	  cld
+.1:
+	  lodsb
+	  test	al,al
+	  jz	.2
+	  mov	[es:edi],al
+	  inc	edi
+	  jmp	.1
+
+.2:
+	  pop	edi
+	  pop	esi
+	  ret
+
+DispAll:
+	  push	ecx
+	  push	edx
+
+	  mov	ah,0ch
+	  mov	dl,al
+	  shr	al,4
+	  mov	ecx,2
+.begin:
+	  and	al,01111b
+	  cmp	al,9
+	  ja	.1
+	  add	al,'0'
+	  jmp	.2
+.1:
+	  sub	al,0ah
+	  add	al,'A'
+.2:
+	  mov	[gs:edi],ax
+	  add	edi,2
+	  mov	al,dl
+	  loop	.begin
+	  add	edi,2
+
+	  pop	edx
+	  pop	ecx
+
+	  ret
+DispReturn:
+	  push	eax
+	  push	ebx
+
+	  mov	eax,edi
+	  mov	bl,160
+	  div	bl
+	  and	eax,0ffh
+	  inc	eax
+	  mov	bl,160
+	  mul	bl
+	  mov	edi,eax
+	  pop	ebx
+	  pop	eax
+	  ret
 Seg32Len		equ		$ - LABEL_SEG_CODE32
+[SECTION .s16code]
+ALIGN 32
+[BITS 16]
+LABEL_SEG_CODE16:
+	  ; jmp back to real mode
+	  mov	ax,SelectorNormal
+	  mov	ds,ax
+	  mov	es,ax
+	  mov	fs,ax
+	  mov	gs,ax
+	  mov	ss,ax
+
+	  mov	eax,cr0
+	  and	al,11111110b
+	  mov	cr0,eax
+
+LABLE_GO_BACK_TO_REAL:
+	  jmp	0:LABEL_REAL_ENTRY
+
+Code18Len	equ		$ - LABEL_SEG_CODE16
+
