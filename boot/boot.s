2ec8f4d28e00017de9639212a116af19cce2455b
soildfaker <enbinli@outlook.com>
2017-12-10 00:12:34 +0800

    asm hello, world

--- boot/boot.s
+++ boot/boot.s
@@ -1,20 +1,31 @@
-.org 0x7c00
-  movw    %cs, %ax
-  movw    %ax, %ds
-  movw    %ax, %es
-  call    DispStr
-  jmp     .
-DispStr:
-  movw    $msg, %ax
-  movw    %ax, %bp
-  movw    $len, %cx
-  movw    $0x1301, %ax
-  movw    $0x000c, %bx
-  movb    $0, %dl
-  int     $0x10
-  ret
-  msg : .string "Hello Worls!\\n"
-  len = . - msg
-.fill 510-(.-$0x7c00),1,0
-.long 0xaa55
+#generate 16-bit code
+.code16
 
+.text
+.global _start
+
+_start:
+  jmp _boot
+  msg: .asciz "Hello, World\r\n"
+
+  .macro putStr str
+    leaw  \str, %si
+    call  putChar
+  .endm
+
+  putChar:
+    lodsb
+    orb %al, %al
+    jz  out
+    movb $0x0e, %ah
+    int  $0x10
+    jmp putChar
+  out:
+  ret
+  
+_boot:
+  putStr msg
+
+  . = _start + 510    #mov to 510th byte from 0 pos
+  .byte 0x55           #append boot signature
+  .byte 0xaa           #append boot signature
