4a2c5bf1ed44ff7a108c0dd092d9e303b9e70e07
soildfaker <enbinli@outlook.com>
2017-12-10 16:02:07 +0800

    update ASM bootloader

--- boot/boot.s
+++ boot/boot.s
@@ -6,26 +6,46 @@
 
 _start:
   jmp _boot
-  msg: .asciz "Hello, World\r\n"
 
-  .macro putStr str
-    leaw  \str, %si
-    call  putChar
-  .endm
-
-  putChar:
-    lodsb
-    orb %al, %al
-    jz  out
-    movb $0x0e, %ah
-    int  $0x10
-    jmp putChar
-  out:
-  ret
-  
 _boot:
-  putStr msg
+  movw $0x07c0, %ax
+  movw %ax, %ds
+  lgdt gdt_prt
 
-  . = _start + 510    #mov to 510th byte from 0 pos
-  .byte 0x55           #append boot signature
-  .byte 0xaa           #append boot signature
+  cli
+  in   $0x92, %al
+  or   $0x2, %al
+  out  %al, $0x92
+  movl %cr0, %eax
+  or   $1, %eax
+  movl %eax, %cr0
+
+print:
+  jmp .
+
+msg: .asciz "Hello, World\r\n"
+msg2: .asciz "Protect Mode Now...\r\n"
+
+gdt:
+  .word 0,0,0,0 # descriptor 0 deprecated
+
+  # descriptor 1
+  .word 0x07ff  # section limit
+  .word 0x0000  # section base
+  .word 0x9a00  # code scetion read/execute
+  .word 0x00c0
+
+  .word 0xb800  # section limit
+  .word 0x0000  # section base
+  .word 0x9a00  # data scetion read/write
+  .word 0x00c0
+
+gdt_len:
+  .set GDT_SIZE, . - gdt
+
+gdt_prt: 
+  .word GDT_SIZE - 1
+  .long 0
+
+.org 510
+  .word 0xaa55
