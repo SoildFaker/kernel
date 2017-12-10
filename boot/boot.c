35a9b110f1d309784eae33ea0aaf63b839a31ef1
soildfaker <enbinli@outlook.com>
2017-12-10 13:58:50 +0800

    init gdt

--- boot/boot.c
+++ boot/boot.c
@@ -1,3 +1,6 @@
+#include <tinx/types.h>
+#include <boot/gdt.h>
+
 __asm__(".code16gcc\n");
 __asm__("jmpl $0x0000, $main\n");
 
@@ -11,5 +14,13 @@ void printStr(const char* pStr) {
 }
 
 void main(){
+  struct gdt gdt_table[5];
+
+  set_gdt(&gdt_table[0], 0, 0, 0, 0);
+  set_gdt(&gdt_table[1], 0, 0xffff, 0x9a, 0x0f);
+  set_gdt(&gdt_table[2], 0, 0xffff, 0x92, 0x0f);
+  set_gdt(&gdt_table[3], 0, 0xffff, 0xfa, 0x0f);
+  set_gdt(&gdt_table[4], 0, 0xffff, 0xf2, 0x0f);
+
   printStr("Hello, World");
 }
