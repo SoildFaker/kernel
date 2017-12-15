44413be6249ca33d9d53cf90f5451e1e15471198
soildfaker <enbinli@outlook.com>
2017-12-15 12:27:37 +0800

    function for memory operation

---
+++ include/string.h
@@ -0,0 +1,14 @@
+#ifndef INCLUDE_STRING_H_
+#define INCLUDE_STRING_H_
+
+#include "types.h"
+#include "display.h"
+
+void memcpy(u8 *dest, const u8 *src, u32 len);
+void memset(u8 *dest, u8 val, u32 len);
+int strcmp(const char *str1, const char *str2);
+char *strcpy(char *dest, const char *src);
+char *strcat(char *dest, const char *src);
+int strlen(const char *src);
+
+#endif // INCLUDE_STRING_H_
