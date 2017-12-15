e9cb6d3b152435816650f1180cd759a86c660269
soildfaker <enbinli@outlook.com>
2017-12-15 12:26:18 +0800

    change kernel size

--- boot/boot.s
+++ boot/boot.s
@@ -12,26 +12,26 @@ boot:
 go: movw %cs, %ax # make segment register point to 0x07c0
   movw %ax, %ds
   movw %ax, %ss
-  movw $0x0400, %sp       # stack pointer prepare for copy kernel
+  movw $0x0fff, %sp       # stack pointer prepare for copy kernel
 
 load_system:
   movw $0x0000, %dx       # bios int 0x13 read data from disk
   movw $0x0002, %cx       # second sector
-  movw $0x1000, %ax       # move to 0x1000 in menory
+  movw $0x2000, %ax       # move to 0x1000 in menory
   movw %ax, %es           # ES:BX point to buffer
   xor  %bx, %bx
-  movw $0x0200+17, %ax   # AH function index AL number of sector
+  movw $0x0200+32, %ax   # AH function index AL number of sector
   int  $0x13
   jnc  ok_load
 die:  jmp  die
 
 ok_load:
   cli                     # interrupt closed
-  movw $0x1000, %ax       # where to copy
+  movw $0x2000, %ax       # where to copy
   movw %ax, %ds
   xor  %ax, %ax
   movw %ax, %es
-  movw $0x1000, %cx       # copy 0x1000 times
+  movw $0x2000, %cx       # copy 0x1000 times
   subw %si, %si
   subw %di, %di
   rep  movsw
