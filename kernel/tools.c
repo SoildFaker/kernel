c7c693f9143ccd3dca519f7d8522d7feb8ffae08
soildfaker <enbinli@outlook.com>
2017-12-15 12:28:29 +0800

    printf function test

--- kernel/tools.c
+++ kernel/tools.c
@@ -1,112 +1,290 @@
-#include <tools.h>
-#include <common.h>
-#include <types.h>
+#include "tools.h"
+#include "display.h"
+#include "common.h"
+#include "types.h"
+#include "string.h"
 
-volatile u8 cursor_x = 0;
-volatile u8 cursor_y = 0;
-volatile u16 *video_buffer = (volatile u16*)0xB8000;
-
-static void flush_cursor()
+void kprint(const char *format, ...)
 {
-  // for default vga support the screen width = 80
-  u16 cursor_location = cursor_y * 80 + cursor_x;
+	// 避免频繁创建临时变量，内核的栈很宝贵
+	static char buff[1024];
+	va_list args;
+	int i;
 
-  outb(14, 0x3d4); // set vga cursor high 8 bit
-  outb(cursor_location >> 8, 0x3d5); // send cursor high 8 bit
-  outb(15, 0x3d4); // set vga cursor low 8 bit
-  outb(cursor_location, 0x3d5); // send cursor low 8 bit
+	va_start(args, format);
+	i = vsprintf(buff, format, args);
+	va_end(args);
+
+	buff[i] = '\0';
+
+	display_print(buff);
 }
 
-void flush_line(u8 line)
+void kprint_color(u8 back, u8 fore, const char *format, ...)
 {
-  u8 attribute_byte = (COLOR_BLACK << 4) | (COLOR_WHITE);
-  u16 blank = 0x20 | (attribute_byte << 8);
-  u16 i;
-  for (i=0; i<80; i++){
-    video_buffer[line*80+i] = blank;
-  }
-  cursor_x = 0;
-  flush_cursor();
+	// 避免频繁创建临时变量，内核的栈很宝贵
+	static char buff[1024];
+	va_list args;
+	int i;
+
+	va_start(args, format);
+	i = vsprintf(buff, format, args);
+	va_end(args);
+
+	buff[i] = '\0';
+
+	display_print_color(buff, fore, back);
 }
 
-void flush_screen()
+static int skip_atoi(const char **s)
 {
-  u8 attribute_byte = (COLOR_BLACK << 4) | (COLOR_WHITE);
-  u16 blank = 0x20 | (attribute_byte << 8);
-  u16 i;
-  for (i=0; i<80*25; i++){
-    video_buffer[i] = blank;
-  }
-  cursor_x = 0;
-  cursor_y = 0;
-  flush_cursor();
+	int i = 0;
+
+	while (is_digit(**s)) {
+		i = i * 10 + *((*s)++) - '0';
+	}
+
+	return i;
 }
 
-static void scroll()
+static char *number(char *str, int num, int base, int size, int precision, int type)
 {
-  if (cursor_y > 24) {
-    u16 i;
+	char c, sign, tmp[36];
+	const char *digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
 
-    for (i = 0 * 80; i < 24 * 80; i++) {
-      video_buffer[i] = video_buffer[i+80];
-    }
-    flush_line(24);
+	if (type & SMALL) {
+		digits ="0123456789abcdefghijklmnopqrstuvwxyz";
+	}
+	if (type & LEFT) {
+		type &= ~ZEROPAD;
+	}
+	if (base < 2 || base > 36) {
+		return 0;
+	}
 
-    cursor_y = 24;
-  }
+	c = (type & ZEROPAD) ? '0' : ' ' ;
+
+	if (type & SIGN && num < 0) {
+		sign = '-';
+		num = -num;
+	} else {
+		sign = (type&PLUS) ? '+' : ((type&SPACE) ? ' ' : 0);
+	}
+
+	if (sign) {
+	      size--;
+	}
+	if (type & SPECIAL) {
+		if (base == 16) {
+			size -= 2;
+		} else if (base == 8) {
+			size--;
+		}
+	}
+	i = 0;
+	if (num == 0) {
+		tmp[i++] = '0';
+	} else {
+		while (num != 0) {
+			tmp[i++] = digits[do_div(num,base)];
+		}
+	}
+
+	if (i > precision) {
+		precision = i;
+	}
+	size -= precision;
+
+	if (!(type&(ZEROPAD+LEFT))) {
+		while (size-- > 0) {
+			*str++ = ' ';
+		}
+	}
+	if (sign) {
+		*str++ = sign;
+	}
+	if (type & SPECIAL) {
+		if (base == 8) {
+			*str++ = '0';
+		} else if (base == 16) {
+			*str++ = '0';
+			*str++ = digits[33];
+		}
+	}
+	if (!(type&LEFT)) {
+		while (size-- > 0) {
+			*str++ = c;
+		}
+	}
+	while (i < precision--) {
+		*str++ = '0';
+	}
+	while (i-- > 0) {
+		*str++ = tmp[i];
+	}
+	while (size-- > 0) {
+		*str++ = ' ';
+	}
+
+	return str;
 }
 
-void kputc(char c, u8 fg, u8 bg)
+int vsprintf(char *buff, const char *format, va_list args)
 {
-  u16 attribute = (u16)(fg | bg << 4)<<8;
+	int len;
+	int i;
+	char *str;
+	char *s;
+	int *ip;
 
-  if (c == 0x08 && cursor_x) {
-    // backspace
-    cursor_x--;
-  } else if (c == 0x09) {
-    //tab
-    cursor_x = (cursor_x+8) & ~(8-1);
-  } else if (c == '\r') {
-    cursor_x = 0;
-  } else if (c == '\n') {
-    cursor_x = 0;
-    cursor_y++;
-  } else if (c >= ' ') {
-    video_buffer[cursor_y*80 + cursor_x] = c | attribute;
-    cursor_x++;
-  }
- 
-  // start new line when cursor_x > 79
-  if (cursor_x > 79) {
-    cursor_x = 0;
-    cursor_y ++;
-  }
- 
-  scroll();
-  flush_cursor();
-}
+	int flags;		// flags to number()
 
-void kprintf(const char *string)
-{
-  while (*string) {
-    kputc(*string++, COLOR_WHITE, COLOR_BLACK);
-  }
-}
-void kprint_hex(u32 hex)
-{
-  char *pc = (char *)"0x00000000";
-  char *pc_t = pc;
-  pc += 10;
-  do{
-    *(--pc) = "0123456789abcdef"[hex % 16];
-  }while(hex/=16);
-  pc = pc_t;
-  kprintf(pc);
-}
+	int field_width;	// width of output field
+	int precision;		// min. # of digits for integers; max number of chars for from string
 
-void kprint_color(const char *string, u8 fg, u8 bg)
-{
-  while (*string) {
-    kputc(*string++, fg, bg);
-  }
+	for (str = buff ; *format ; ++format) {
+		if (*format != '%') {
+			*str++ = *format;
+			continue;
+		}
+			
+		flags = 0;
+		repeat:
+			++format;		// this also skips first '%'
+			switch (*format) {
+				case '-': flags |= LEFT;
+					  goto repeat;
+				case '+': flags |= PLUS;
+					  goto repeat;
+				case ' ': flags |= SPACE;
+					  goto repeat;
+				case '#': flags |= SPECIAL;
+					  goto repeat;
+				case '0': flags |= ZEROPAD;
+					  goto repeat;
+			}
+		
+		// get field width
+		field_width = -1;
+		if (is_digit(*format)) {
+			field_width = skip_atoi(&format);
+		} else if (*format == '*') {
+			// it's the next argument
+			field_width = va_arg(args, int);
+			if (field_width < 0) {
+				field_width = -field_width;
+				flags |= LEFT;
+			}
+		}
+
+		// get the precision
+		precision = -1;
+		if (*format == '.') {
+			++format;	
+			if (is_digit(*format)) {
+				precision = skip_atoi(&format);
+			} else if (*format == '*') {
+				// it's the next argument
+				precision = va_arg(args, int);
+			}
+			if (precision < 0) {
+				precision = 0;
+			}
+		}
+
+		// get the conversion qualifier
+		//int qualifier = -1;	// 'h', 'l', or 'L' for integer fields
+		if (*format == 'h' || *format == 'l' || *format == 'L') {
+			//qualifier = *format;
+			++format;
+		}
+
+		switch (*format) {
+		case 'c':
+			if (!(flags & LEFT)) {
+				while (--field_width > 0) {
+					*str++ = ' ';
+				}
+			}
+			*str++ = (unsigned char) va_arg(args, int);
+			while (--field_width > 0) {
+				*str++ = ' ';
+			}
+			break;
+
+		case 's':
+			s = va_arg(args, char *);
+			len = strlen(s);
+			if (precision < 0) {
+				precision = len;
+			} else if (len > precision) {
+				len = precision;
+			}
+
+			if (!(flags & LEFT)) {
+				while (len < field_width--) {
+					*str++ = ' ';
+				}
+			}
+			for (i = 0; i < len; ++i) {
+				*str++ = *s++;
+			}
+			while (len < field_width--) {
+				*str++ = ' ';
+			}
+			break;
+
+		case 'o':
+			str = number(str, va_arg(args, unsigned long), 8,
+				field_width, precision, flags);
+			break;
+
+		case 'p':
+			if (field_width == -1) {
+				field_width = 8;
+				flags |= ZEROPAD;
+			}
+			str = number(str, (unsigned long) va_arg(args, void *), 16,
+				field_width, precision, flags);
+			break;
+
+		case 'x':
+			flags |= SMALL;
+		case 'X':
+			str = number(str, va_arg(args, unsigned long), 16,
+				field_width, precision, flags);
+			break;
+
+		case 'd':
+		case 'i':
+			flags |= SIGN;
+		case 'u':
+			str = number(str, va_arg(args, unsigned long), 10,
+				field_width, precision, flags);
+			break;
+		case 'b':
+			str = number(str, va_arg(args, unsigned long), 2,
+				field_width, precision, flags);
+			break;
+
+		case 'n':
+			ip = va_arg(args, int *);
+			*ip = (str - buff);
+			break;
+
+		default:
+			if (*format != '%')
+				*str++ = '%';
+			if (*format) {
+				*str++ = *format;
+			} else {
+				--format;
+			}
+			break;
+		}
+	}
+	*str = '\0';
+
+	return (str -buff);
 }
