6e85556ca0d6cfa372ce0eff96f2f3462f1ac968
soildfaker <enbinli@outlook.com>
2017-12-16 18:29:02 +0800

    just jmp to kernel main function

--- kernel/head.s
+++ kernel/head.s
@@ -1,37 +1,6 @@
-.code16 
 
 start:
-  lgdt gdt_ptr           # load the new gdt pointer
-  lidt idt_ptr
-# prepare to protect move
-  movl %cr0, %eax
-  or   $0x01, %al
-  movl %eax, %cr0
-
-# enable A20 line 
-  in  $0x92, %al
-  or  $0x02, %al
-  out %al, $0x92
-
-  movw $0x10, %ax
+  movw $0x18, %ax
   movw %ax, %ss
-  movw %ax, %ds
-  movw %ax, %es
-  movw %ax, %fs
-  movw %ax, %gs
+  jmp $0x8, $kmain
 
-# switch to protect mode
-  jmp  $0x8, $move_kernel
-
-gdt: 
-  .word 0,0,0,0
-  .word 0x07ff, 0x7e00, 0x9a00, 0x00c0    # code segment
-  .word 0x07ff, 0x7e00, 0x9200, 0x00c0    # data segment
-  .word 0x07ff, 0x0000, 0x9a10, 0x00c0    # second code segment
-  .word 0x07ff, 0x0000, 0x9210, 0x00c0    # second data segment
-  .word 0x07ff, 0x0000, 0x9300, 0x00c0    # second data segment
-idt_ptr:
-  .word 0, 0, 0
-
-gdt_ptr:
-  .word 0x7fff, 0x7e00+gdt, 0
