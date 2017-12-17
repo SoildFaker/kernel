93a0893730a20742d9232aaa27469ece353b281c
soildfaker <enbinli@outlook.com>
2017-12-17 14:36:37 +0800

    int 0x15 stored memory map at 0x1000

--- boot/loader.s
+++ boot/loader.s
@@ -1,5 +1,46 @@
+.code16
 
 start:
+# use the INT 0x15, eax= 0xE820 BIOS function to get a memory map
+# inputs: es:di -> destination buffer for 24 byte entries
+# outputs: bp = entry count, trashes all registers except esi
+get_memory_map:
+  movw $0, %eax 
+  movw %ax, %es
+  movw $0x1000, %di 
+_loop:
+  movw $0xe820, %eax 
+  movw $20, %ecx
+  movl $0x534d4150, %edx
+  int  $0x15 
+  jc   _failed 
+  add  $20, %di 
+  incl (0x0500)
+  cmp  $0, %ebx
+  jne  _loop
+  jmp  protected_mode
+
+_failed:
+  movl $0, %es:(%di)
+
+protected_mode:
+  lgdt gdt_ptr           # load the new gdt pointer
+
+# prepare to protect move
+  movl %cr0, %eax
+  or   $0x01, %al
+  movl %eax, %cr0
+
+# enable A20 line 
+  in  $0x92, %al
+  or  $0x02, %al
+  out %al, $0x92
+
+
+  jmp $0x8, $move_kernel
+
+.code32
+move_kernel:
   cli                     # interrupt closed
   movw $0x20, %ax         # address copy from selector point to 0x9000
   movw %ax, %ds
@@ -16,4 +57,15 @@ start:
   movw %ax, %fs
   movw %ax, %gs
   jmp $0x8, $0x100000
+
+gdt: 
+  .word 0,0,0,0
+  .word 0x07ff, 0x0000, 0x9a00, 0x00c0    # 0x08 code segment
+  .word 0x07ff, 0x0000, 0x9200, 0x00c0    # 0x10 data segment
+  .word 0x07ff, 0x0000, 0x9210, 0x00c0    # 0x18 data segment
+  .word 0x07ff, 0x9000, 0x9200, 0x00c0    # 0x20 data segment
+
+gdt_ptr:
+  .word 0x7fff, gdt, 0
+
 
