4cb4060c8f585ee56304c25444b61456533ed72b
soildfaker <enbinli@outlook.com>
2017-12-17 14:37:00 +0800

    real mode simple loader

--- boot/boot.s
+++ boot/boot.s
@@ -33,31 +33,8 @@ load_disk:
 die:  jmp  die
 
 ok_loaded:
+  jmp $0x0000, $0x8000
 
-  lgdt gdt_ptr           # load the new gdt pointer
-
-# prepare to protect move
-  movl %cr0, %eax
-  or   $0x01, %al
-  movl %eax, %cr0
-
-# enable A20 line 
-  in  $0x92, %al
-  or  $0x02, %al
-  out %al, $0x92
-
-
-  jmp $0x8, $0x8000
-
-gdt: 
-  .word 0,0,0,0
-  .word 0x07ff, 0x0000, 0x9a00, 0x00c0    # 0x08 code segment
-  .word 0x07ff, 0x0000, 0x9200, 0x00c0    # 0x10 data segment
-  .word 0x07ff, 0x0000, 0x9210, 0x00c0    # 0x18 data segment
-  .word 0x07ff, 0x9000, 0x9200, 0x00c0    # 0x20 data segment
-
-gdt_ptr:
-  .word 0x7fff, gdt, 0
 
 .org 510
   .word 0xaa55
