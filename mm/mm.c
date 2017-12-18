ad43b8cac0b46e4b73534289c395ed497040439c
soildfaker <enbinli@outlook.com>
2017-12-18 17:05:24 +0800

    update memory manger print string

--- mm/mm.c
+++ mm/mm.c
@@ -8,6 +8,7 @@ u32 *count = (u32 *)0x500;
 void show_memory_map()
 {
   u8 i=0;
+  kprint("----------MEMORY MAP----------\n");
   for (i=0;i<*count;i++){
     kprint("BASE: 0x%09X\tLENGTH: 0x%09X\tTYPE:0x%01X\n", 
       (mmap->BaseL| mmap->BaseH<<8), 
