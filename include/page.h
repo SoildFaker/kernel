069cf59a2a0bd9d4a4de5bb69a560c162b40a637
soildfaker <enbinli@outlook.com>
2017-12-20 20:24:25 +0800

    page attribute defination added

--- include/page.h
+++ include/page.h
@@ -5,10 +5,25 @@
 #include "init.h"
 #include "types.h"
 
-#define  PT_PRESENT 0x01
-#define  PT_READWRITE 0x02
+#define  PG_PRESENT   0x001
+#define  PG_WRITE     0x002
+#define  PG_USER      0x004
+#define  PG_ACCESSED  0x020
+#define  PG_DIRTY     0x040
+#define  PG_SIZE      0x080
+#define  PAGE_SIZE    0x1000
+#define  PAGE_MASK    0xFFFFF000
 
-#define PAGE_SIZE 4096
+#define  PDT_COUNT    16
+#define  KERNV_START  0x100000
+#define  KERNP_START  0x100000
+
+// 获取一个地址的页目录项
+#define PGD_INDEX(x) (((x) >> 22) & 0x3FF)
+// 获取一个地址的页表项
+#define PTE_INDEX(x) (((x) >> 12) & 0x3FF)
+// 获取一个地址的页內偏移
+#define OFFSET_INDEX(x) ((x) & 0xFFF)
 
 struct page_entry {
   u32 flags  : 9;  // page flags
@@ -17,9 +32,10 @@ struct page_entry {
 };
 typedef struct page_entry page_entry_t;
 
-__attribute__((aligned(PAGE_SIZE))) page_entry_t pdt[1024];
-__attribute__((aligned(PAGE_SIZE))) page_entry_t pet[1024];
-
+void switch_pgd(u32 pd);
+void map(page_entry_t *pgd_now, u32 va, u32 pa, u32 flags);
+void unmap(page_entry_t *pgd_now, u32 va);
+u32 get_mapping(page_entry_t *pgd_now, u32 va, u32 *pa);
 void flush_page_directory(page_entry_t *directory);
 void enable_page();
 void init_page();
