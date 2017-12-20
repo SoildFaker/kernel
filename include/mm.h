ecfd4c80bf2d3c6f9fea4b6ab4bb10a508882e8b
soildfaker <enbinli@outlook.com>
2017-12-20 16:24:06 +0800

    kernel memory alloc function kmalloc added, for now still didn't have any idea about paging and memory managing

--- include/mm.h
+++ include/mm.h
@@ -13,8 +13,18 @@ struct mmap_entry {
 }__attribute__((packed));
 typedef struct mmap_entry mmap_entry_t;
 
+extern u8 kernel_start[];
+extern u8 kernel_end[];
+
 extern mmap_entry_t *mmap;
 extern u32 *count;
 
 void show_memory_map();
+
+u32 kmalloc_attr(u32 size, u8 align, u32* phys);
+u32 kmalloc_a(u32 size);
+u32 kmalloc_p(u32 size, u32 *phys);
+u32 kmalloc_ap(u32 size, u32 *phys);
+u32 kmalloc(u32 size);
+
 #endif
--- mm/mm.c
+++ mm/mm.c
@@ -5,6 +5,8 @@
 mmap_entry_t *mmap = (mmap_entry_t *)0x1000;
 u32 *count = (u32 *)0x500;
 
+u32 placement_address = (u32)kernel_end;
+
 void show_memory_map()
 {
   u8 i=0;
@@ -19,3 +21,36 @@ void show_memory_map()
 
 }
 
+u32 kmalloc_attr(u32 size, u8 align, u32* phys)
+{
+  if (align == 1 && (placement_address & 0xfffff000)){
+    placement_address &= 0xfffff000;
+    placement_address += 0x1000;
+  }
+  if(phys){
+    *phys = placement_address;
+  }
+  u32 temp = placement_address;
+  placement_address += size;
+  return temp;
+}
+
+u32 kmalloc_a(u32 size)
+{
+  return kmalloc_attr(size, 1, 0);
+}
+
+u32 kmalloc_p(u32 size, u32 *phys)
+{
+  return kmalloc_attr(size, 0, phys);
+}
+
+u32 kmalloc_ap(u32 size, u32 *phys)
+{
+  return kmalloc_attr(size, 1, phys);
+}
+
+u32 kmalloc(u32 size)
+{
+  return kmalloc_attr(size, 0, 0);
+}
