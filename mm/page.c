9b3a4d4f65a03469960640bf10d6b6b0c9b67479
soildfaker <enbinli@outlook.com>
2017-12-20 20:18:39 +0800

    using kmalloc to initilize page directory and page entry table

--- mm/page.c
+++ mm/page.c
@@ -1,36 +1,118 @@
 #include "types.h"
 #include "init.h"
 #include "page.h"
+#include "mm.h"
 #include "display.h"
 #include "tools.h"
 
+page_entry_t *pdt;
+static page_entry_t **pet;
+
 u32 page_addr;
 u32 phy_addr = 0x0;;
+
 void init_page()
 {
+  pdt = (page_entry_t *)kmalloc_a(sizeof(page_entry_t)*1024);
+  pet = (page_entry_t **)kmalloc_a(sizeof(page_entry_t *)*PDT_COUNT);
+
   u32 i;
+  for (i=0; i<PDT_COUNT; i++){
+    pet[i] = (page_entry_t *)kmalloc_a(sizeof(page_entry_t)*1024);
+
+  }
   for(i=0; i<1024;i++){
     phy_addr = 0x000000 + i*0x1000;
-    pet[i].base = (phy_addr>>12);
-    pet[i].flags = PT_PRESENT | PT_READWRITE;
+    pet[0][i].base = (phy_addr>>12);
+    pet[0][i].flags = PG_PRESENT | PG_WRITE;
   }
-  pdt[0].base = (u32)(&pet)>>12;
-  pdt[0].flags = PT_PRESENT | PT_READWRITE;
+  for(i=0; i<1024;i++){
+    phy_addr = 0x400000 + i*0x1000;
+    pet[1][i].base = (phy_addr>>12);
+    pet[1][i].flags = PG_PRESENT | PG_WRITE;
+  }
+  pdt[0].base = (u32)(pet[0])>>12;
+  pdt[0].flags = PG_PRESENT | PG_WRITE;
+  pdt[1].base = (u32)(pet[1])>>12;
+  pdt[1].flags = PG_PRESENT | PG_WRITE;
 
+  /*kprint("%x", (pet[0][0x100]));*/
   register_interrupt_handler(14, (interrupt_handler_t)page_fault);
 
   flush_page_directory(pdt);
   enable_page();
 }
 
-void page_alloc()
+/*void map(page_entry_t *pdt_now, u32 va, u32 pa, u32 flags)*/
+/*{*/
+  /*u32 pdt_idx = PGD_INDEX(va);*/
+  /*u32 pet_idx = PTE_INDEX(va);*/
+  
+  /*page_entry_t *pet_now = (page_entry_t *)(pdt_now[pdt_idx].base);*/
+  /*if (!pet_now) {*/
+    /*pet_now->base = pmm_alloc_page();*/
+    /*pdt_now[pdt_idx].base = (u32)pet_now;*/
+    /*pdt_now[pdt_idx].flags = PG_PRESENT | PG_WRITE;*/
+    
+    /*// 转换到内核线性地址并清 0*/
+    /*pte = (page_entry_t *)((u32)pte + PAGE_OFFSET);*/
+    /*bzero(pte, PAGE_SIZE);*/
+  /*} else {*/
+    /*// 转换到内核线性地址*/
+    /*pte = (page_entry_t *)((u32)pte + PAGE_OFFSET);*/
+  /*}*/
+  
+  /*pte[pte_idx] = (pa & PAGE_MASK) | flags;*/
+  
+  /*// 通知 CPU 更新页表缓存*/
+  /*asm volatile ("invlpg (%0)" : : "a" (va));*/
+/*}*/
+
+/*void unmap(page_entry_t *pgd_now, u32 va)*/
+/*{*/
+  /*u32 pgd_idx = PGD_INDEX(va);*/
+  /*u32 pte_idx = PTE_INDEX(va);*/
+  
+  /*page_entry_t *pte = (page_entry_t *)(pgd_now[pgd_idx] & PAGE_MASK);*/
+  
+  /*if (!pte) {*/
+    /*return;*/
+  /*}*/
+  
+  /*// 转换到内核线性地址*/
+  /*pte = (page_entry_t *)((u32)pte + PAGE_OFFSET);*/
+  
+  /*pte[pte_idx] = 0;*/
+  
+  /*// 通知 CPU 更新页表缓存*/
+  /*asm volatile ("invlpg (%0)" : : "a" (va));*/
+/*}*/
+
+/*u32 get_mapping(page_entry_t *pgd_now, u32 va, u32 *pa)*/
+/*{*/
+  /*u32 pgd_idx = PGD_INDEX(va);*/
+  /*u32 pte_idx = PTE_INDEX(va);*/
+  
+  /*page_entry_t *pte = (page_entry_t *)(pgd_now[pgd_idx] & PAGE_MASK);*/
+  /*if (!pte) {*/
+    /*return 0;*/
+  /*}*/
+
+  /*// 转换到内核线性地址*/
+  /*pte = (pte_t *)((u32)pte + PAGE_OFFSET);*/
+  
+  /*// 如果地址有效而且指针不为，则返回地址NULL*/
+  /*if (pte[pte_idx] != 0 && pa) {*/
+    /**pa = pte[pte_idx] & PAGE_MASK;*/
+    /*return 1;*/
+  /*}*/
+
+  /*return 0;*/
+/*}*/
+
+void switch_pgd(u32 pd)
 {
-
-}
-
-void page_free()
-{
-
+  asm volatile ("mov %0, %%cr3" : : "r" (pd));
 }
 
 void flush_page_directory(page_entry_t *pgdir) {
