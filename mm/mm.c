86a44af8255c19f241b22eefe96edfe9e72a0146
soildfaker <enbinli@outlook.com>
2017-12-27 14:15:34 +0800

    kmalloc function worked now

--- mm/mm.c
+++ mm/mm.c
@@ -6,46 +6,28 @@
 mmap_entry_t *mmap = (mmap_entry_t *)0x1000;
 u32 *count = (u32 *)0x500;
 
-memory_header_t *heap_first;
+memory_header_t *heap_first = 0;
 // 申请内存块
 static void alloc_chunk(u32 start, u32 len);
-
 static void free_chunk(memory_header_t *chunk);
-
 static void split_chunk(memory_header_t *chunk, u32 len);
-
 static void glue_chunk(memory_header_t *chunk);
 
 static u32 heap_max = HEAP_START;
 
-static u32 *pmm_stack;
+static u32 pmm_stack[PAGE_MAX_NUM];
 static u32 pmm_stack_top = 0;
 u32 phy_page_count;
 
-void show_memory_map()
-{
-  u8 i=0;
-  mmap_entry_t *map_entry = mmap;
-  kprint("----------MEMORY MAP----------\n");
-  for (i = 0; i < *count; i++){
-    kprint("BASE: 0x%09X\tLENGTH: 0x%09X\tTYPE:0x%01X\n", 
-      ((map_entry+i)->base_low | (map_entry+i)->base_high<<8), 
-      ((map_entry+i)->length_low | (map_entry+i)->length_high<<8), 
-      (map_entry+i)->type);
-  }
-}
-
 void init_pmm()
 {
-  pmm_stack = (u32 *)kmalloc(sizeof(u32)*(PAGE_MAX_NUM+1));
   mmap_entry_t *map_entry = mmap;
 
   u8 i;
-  for (i = 0; i < *count; i++){
+  for (i = 1; i < *count; i++){
     // 如果是可用内存 ( 按照协议，1 表示可用内存，其它数字指保留区域 )
     if ((map_entry+i)->type == 0x1){
       // 把内核结束位置到结束位置的内存段，按页存储到页管理栈里
-      // 最多支持512MB的物理内存
       u32 page_addr = (map_entry+i)->base_low;
       u32 length = (map_entry+i)->base_low + (map_entry+i)-> length_low;
 
@@ -127,8 +109,8 @@ void alloc_chunk(u32 start, u32 len)
   // 如果当前堆的位置已经到达界限则申请内存页
   // 必须循环申请内存页直到有到足够的可用内存
   while (start + len > heap_max) {
-    /*u32 page = pmm_alloc_page();*/
-    /*map(pgd_kern, heap_max, page, PAGE_PRESENT | PAGE_WRITE);*/
+    u32 page = pmm_alloc_page();
+    map(kpdt, heap_max, page, PG_PRESENT | PG_WRITE);
     heap_max += PAGE_SIZE;
   }
 }
@@ -145,8 +127,8 @@ void free_chunk(memory_header_t *chunk)
   while ((heap_max - PAGE_SIZE) >= (u32)chunk) {
     heap_max -= PAGE_SIZE;
     u32 page;
-    /*get_mapping(pgd_kern, heap_max, &page);*/
-    /*unmap(pgd_kern, heap_max);*/
+    get_mapping(kpdt, heap_max, &page);
+    unmap(kpdt, heap_max);
     pmm_free_page(page);
   }
 }
