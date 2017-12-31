a95b9548a4fa71e0a61b3d0f202203d88863beef
soildfaker <enbinli@outlook.com>
2017-12-31 22:04:54 +0800

    bootloader modified: boot ELF kernel file from disk

--- boot/boot.s
+++ boot/boot.s
@@ -1,22 +1,36 @@
-#generate 16-bit code
-.code16
+.code16           # Assemble for 16-bit code
 
-.text
-.global _start
-
-_start:
-
-  jmp boot
-boot:
+start:
   movw %cs, %ax # make segment register point to 0x07c0
   movw %ax, %ds
   movw %ax, %ss
   movw $0x0fff, %sp       # stack pointer prepare for copy kernel
 
+# Store memory map info 
+# It will save memory map at 
+get_memory_map:
+  movw $0, %ax 
+  movw %ax, %es
+  movw $0x1000, %di 
+_loop:
+  movw $0xe820, %ax 
+  movw $20, %cx
+  movl $0x534d4150, %edx
+  int  $0x15 
+  jc   _failed 
+  add  $20, %di 
+  incl (0x0500)
+  cmp  $0, %ebx
+  jne  _loop
+  jmp  load_disk
+
+_failed:
+  movl $0, %es:(%di)
+
 load_disk:
-  movw $0x0000, %dx       # bios int 0x13 read data from disk
+  movw $0x0080, %dx       # bios int 0x13 read data from disk
   movw $0x0002, %cx       # second sector
-  movw $0x0800, %ax       # move to 0x00100000 in menory
+  movw $0x0800, %ax       # move to 0x8000 in menory
   movw %ax, %es           # ES:BX point to buffer
   xor  %bx, %bx
   movw $0x0200+1, %ax   # AH function index AL number of sector
@@ -26,7 +40,6 @@ die:  jmp  die
 
 ok_loaded:
   jmp $0x0000, $0x8000
-
 
 .org 510
   .word 0xaa55
---
+++ boot/loaderasm.s
@@ -0,0 +1,64 @@
+.code16                 # Assemble for 16−bit mode
+
+start:
+  cli                   # Disable BIOS interrupts
+# Zero data segment registers DS, ES, and SS.
+  xorw %ax, %ax # Set %ax to zero
+  movw %ax, %ds # −> Data Segment
+  movw %ax, %es # −> Extra Segment
+  movw %ax, %ss # −> Stack Segment
+
+protected_mode:
+# enable A20 line 
+  in  $0x92, %al
+  or  $0x02, %al
+  out %al, $0x92
+
+  lgdt gdt_ptr           # load the new gdt pointer
+
+# Switch from real to protected mode. Use a bootstrap GDT that makes
+# virtual addresses map directly to physical addresses so that the
+# effective memory map doesn’t change during the transition.
+# prepare to protect move
+  movl %cr0, %eax
+  or   $0x01, %al
+  movl %eax, %cr0
+
+# Complete transition to 32−bit protected mode by using long jmp
+# to reload %cs and %eip. The segment descriptors are set up with no
+# translation, so that the mapping is still the identity mapping.
+  jmp $0x8, $start_32
+
+.code32
+start_32:
+  movw $0x10, %ax 
+  movw %ax, %ds
+  movw %ax, %es
+  movw %ax, %ss
+  #movw $0x0, %ax    # for now don't use fs,gs
+  movw %ax, %fs
+  movw %ax, %gs
+
+  # Set up the stack pointer and call into C.
+  movl $start, %esp
+  call bootmain
+
+  # If bootmain returns (it shouldn’t), trigger a Bochs
+  # breakpoint if running under Bochs, then loop.
+  movw $0x8a00, %ax # 0x8a00 −> port 0x8a00
+  movw %ax, %dx
+  outw %ax, %dx
+  movw $0x8ae0, %ax # 0x8ae0 −> port 0x8a00
+  outw %ax, %dx
+spin:
+  jmp spin
+
+gdt: 
+  .word 0,0,0,0
+  .word 0x07ff, 0x0000, 0x9a00, 0x00c0    # 0x08 code segment
+  .word 0x07ff, 0x0000, 0x9200, 0x00c0    # 0x10 data segment
+
+.p2align 2           # force 4 byte alignment
+gdt_ptr:
+  .word (gdt_ptr - gdt - 1)
+  .long gdt
