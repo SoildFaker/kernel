5cecf087c3f6a38a7960e2fdffa17157c6d28b7a
soildfaker <enbinli@outlook.com>
2018-01-07 19:24:54 +0800

    function bzero added

--- kernel/string.c
+++ kernel/string.c
@@ -2,27 +2,33 @@
 // Copy len bytes from src to dest.
 inline void memcpy(u8 *dest, const u8 *src, u32 len)
 {
-    const u8 *sp = (const u8 *)src;
-    u8 *dp = (u8 *)dest;
-    for(; len != 0; len--) *dp++ = *sp++;
+  const u8 *sp = (const u8 *)src;
+  u8 *dp = (u8 *)dest;
+  for(; len != 0; len--) *dp++ = *sp++;
 }
 
 // Write len copies of val into dest.
 inline void memset(u8 *dest, u8 val, u32 len)
 {
-    u8 *temp = (u8 *)dest;
-    for ( ; len != 0; len--) *temp++ = val;
+  u8 *temp = (u8 *)dest;
+  for ( ; len != 0; len--) *temp++ = val;
+}
+
+// Write len copies of val into dest.
+inline void bzero(u8 *dest, u32 len)
+{
+  memset(dest, 0, len);
 }
 
 // Compare two strings. Should return -1 if 
 // str1 < str2, 0 if they are equal or 1 otherwise.
 inline int strcmp(const char *str1, const char *str2)
 {
-	while (*str1 && *str2 && (*str1++ == *str2++)) ;
+  while (*str1 && *str2 && (*str1++ == *str2++)) ;
 
-	if (*str1 == '\0' && *str2 == '\0') {
-	  return 0;
-	}
+  if (*str1 == '\0' && *str2 == '\0') {
+    return 0;
+  }
 
 	if (*str1 == '\0') {
     return -1;
