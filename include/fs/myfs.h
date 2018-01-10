2b7e50c51a7c4bdef768e9a432276f894057dac0
soildfaker <enbinli@outlook.com>
2018-01-10 20:51:04 +0800

    loader kernel from disk

--- include/fs/myfs.h
+++ include/fs/myfs.h
@@ -5,11 +5,20 @@
 
 #define CLUSTER_SIZE 512
 
-struct myfs_table{
-  u32 *entry_name;
+#define FS_DIR  0
+#define FS_FILE 1
+
+struct myfs_entry {
+  char entry_name[16];
   u8   entry_type;
-  u32  offset;
-  u32  length;
+  u32  child_head_index;
+  u32  next_index;
+  u32  data_sector;
+  u32  data_count;
 };
+
+void find_and_read(const char *name, u8 *dst);
+struct myfs_entry * find_file(const char *name);
+void init_myfs();
 
 #endif
