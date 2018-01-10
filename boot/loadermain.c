5be96888a5d8b9732fc685b0da1d2eadd8d007ab
soildfaker <enbinli@outlook.com>
2018-01-10 15:32:33 +0800

    move hard disk driver to a seperated file

--- boot/loadermain.c
+++ boot/loadermain.c
@@ -7,11 +7,7 @@
 #include "common.h"
 #include "elf.h"
 #include "page.h"
-
-#define SECTSIZE 512
-
-void readseg(u8 *pa, u32 count, u32 offset);
-void early_page_init(void);
+#include "drivers/hd.h"
 
 void bootmain(void)
 {
@@ -21,60 +17,23 @@ void bootmain(void)
   u8 *pa;
   elf = (elf_header_t *)(0x10000); // scratch space
   // Read 1st page off disk
-  readseg((u8 *)elf, 4096, 2 * SECTSIZE);
+  readseg((u8 *)elf, 4096, 2 * SECTOR_SIZE);
   // Is this an ELF executable?
-  if(elf->magic != ELF_MAGIC)
+  if(elf->magic != ELF_MAGIC) {
     return; // let bootasm.S handle error
+  }
   // Load each program segment (ignores ph flags).
   ph = (elf_section_header_t *)((u8*)elf + elf->phoff);
   eph = ph + elf->phnum;
   for(; ph < eph; ph++){
     pa = (u8 *)ph->paddr;
-    readseg(pa, ph->filesz, 2 * SECTSIZE + ph->off);
-    if(ph->memsz > ph->filesz)
-      stosb(pa + ph->filesz, 0, 2 * SECTSIZE + ph->memsz - ph->filesz);
+    readseg(pa, ph->filesz, 2 * SECTOR_SIZE + ph->off);
+    if(ph->memsz > ph->filesz) {
+      stosb(pa + ph->filesz, 0, 2 * SECTOR_SIZE + ph->memsz - ph->filesz);
+    }
   }
   // Call the entry point from the ELF header.
   // Does not return!
   entry = (void(*)(void))(elf->entry);
   entry();
-}
-
-void waitdisk(void)
-{
-  // Wait for disk ready.
-  while((inb(0x1F7) & 0xC0) != 0x40) ;
-}
-
-// Read a single sector at offset into dst.
-void readsect(void *dst, u32 offset)
-{
-  // Issue command.
-  waitdisk();
-  outb(0x1F2, 1); // count = 1
-  outb(0x1F3, offset);
-  outb(0x1F4, offset >> 8);
-  outb(0x1F5, offset >> 16);
-  outb(0x1F6, (offset >> 24) | 0xE0);
-  outb(0x1F7, 0x20); // cmd 0x20 − read sectors
-  // Read data.
-  waitdisk();
-  insl(0x1F0, dst, SECTSIZE/4);
-}
-
-// Read ’count’ bytes at ’offset’ from kernel into physical address ’pa’.
-// Might copy more than asked.
-void readseg(u8* pa, u32 count, u32 offset)
-{
-  u8* epa;
-  epa = pa + count;
-  // Round down to sector boundary.
-  pa -= offset % SECTSIZE;
-  // Translate from bytes to sectors; kernel starts at sector 1.
-  offset = (offset / SECTSIZE) + 1;
-  // If this is too slow, we could read lots of sectors at a time.
-  // We’d write more to memory than asked, but it doesn’t matter −−
-  // we load in increasing order.
-  for(; pa < epa; pa += SECTSIZE, offset++)
-    readsect(pa, offset);
 }
