a17f1d18c5bff62aaee41f62cfe150865ce97f8a
soildfaker <enbinli@outlook.com>
2018-01-13 22:06:36 +0800

    function find kernel.elf move to bootloader

--- boot/loadermain.c
+++ boot/loadermain.c
@@ -5,13 +5,47 @@
 #include "page.h"
 #include "drivers/hd.h"
 #include "fs/myfs.h"
+#include "string.h"
+
+struct myfs_struct *header = NULL;
+struct myfs_entry *entries = NULL;
+
+struct myfs_entry *find_file(const char *name)
+{
+  u8 i;
+  for (i = 0; i < 64; i++) {
+    if (entries[i].entry_type == MYFS_FILE) {
+      if (strcmp(entries[i].entry_name, name) == 0) {
+        return &entries[i];
+      }
+    }
+  }
+  return &entries[0];
+}
+
+void init_myfs(u32 header_offset)
+{
+  header = (struct myfs_struct *)(header_offset);
+  readseg((u8 *)header, sizeof(struct myfs_struct), 10*SECTOR_SIZE);
+
+  if (header->magic != MYFS_MAGIC){
+    return;
+  }
+
+  entries = 
+    (struct myfs_entry *)(header_offset + sizeof(struct myfs_struct));
+  readseg(
+      (u8 *)entries, 
+      (sizeof(struct myfs_entry)*header->entry_num), 
+      (10*SECTOR_SIZE + sizeof(struct myfs_struct))
+      );
+}
 
 void loadermain(void)
 {
   elf_header_t *elf;
-  elf_section_header_t *ph, *eph;
   void (*entry)(void);
-  u8 *pa;
+
   elf = (elf_header_t *)(0x10000); // scratch space
   init_myfs(0x20000);
   struct myfs_entry *tmp = find_file("kernel.elf");
@@ -26,19 +60,8 @@ void loadermain(void)
     return; // let loaderasm.S handle error
   }
 
-  // Load each program segment (ignores ph flags).
-  ph = (elf_section_header_t *)((u8*)elf + elf->phoff);
-  eph = ph + elf->phnum;
-  for(; ph < eph; ph++){
-    pa = (u8 *)ph->paddr;
-    readseg(pa, ph->filesz, elf_sector * SECTOR_SIZE + ph->off);
-    if(ph->memsz > ph->filesz) {
-      stosb(pa + ph->filesz, 0, elf_sector * SECTOR_SIZE + (ph->memsz - ph->filesz));
-    }
-  }
-
   // Call the entry point from the ELF header.
   // Does not return!
-  entry = (void(*)(void))(elf->entry);
+  entry = (void(*)(void))disk_read_elf(elf, elf_sector * SECTOR_SIZE);
   entry();
 }
