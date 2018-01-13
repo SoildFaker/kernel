bf873fd3bb7034c89ef6517b5e79761aeaef4388
soildfaker <enbinli@outlook.com>
2018-01-13 22:15:26 +0800

    myfs adder filesystem header

--- include/fs/myfs.h
+++ include/fs/myfs.h
@@ -5,20 +5,24 @@
 
 #define CLUSTER_SIZE 512
 
-#define FS_DIR  0
-#define FS_FILE 1
+#define MYFS_MAGIC   0xEB001234
+#define MYFS_DIR  1
+#define MYFS_FILE 2
+
+struct myfs_struct {
+  u32 magic;
+  u32 entry_num;
+};
 
 struct myfs_entry {
+  u8   entry_type;        // 0 = Unused otherwise 1 = Dir or 2 = File
   char entry_name[16];
-  u8   entry_type;
+  u32  entry_attribute;
   u32  child_head_index;
   u32  next_index;
   u32  data_sector;
   u32  data_count;
+  u32  data_size;
 };
-
-void find_and_read(const char *name, u8 *dst);
-struct myfs_entry * find_file(const char *name);
-void init_myfs(u32 table_offset);
 
 #endif
--- tools/mkfs.c
+++ tools/mkfs.c
@@ -5,27 +5,39 @@
 #include "common.h"
 
 #define SECTOR_SIZE 512
+#define ENTRY_NUM   64
 
 #define UP_ROUND(x) ((x) + SECTOR_SIZE - ((x) % SECTOR_SIZE))
 #define DOWN_ROUND(x) ((x) - ((x) % SECTOR_SIZE))
 
-struct myfs_entry entrys[64];
+struct myfs_struct header;
+struct myfs_entry entries[ENTRY_NUM];
 
 int main(int argc, char **argv)
 {
   int entry_num = (argc-1)/2;
+
+  // header 
+  header.magic = MYFS_MAGIC;
+  header.entry_num = entry_num;
+
   printf("%d", entry_num);
   int sector_count = 0;
   printf("size of entry %d\n", (int)sizeof(struct myfs_entry));
-  // put entrys at thrid sector
+  // put entries at thrid sector
   sector_count += 10;
   int entry_table_length = (sizeof(struct myfs_entry) * 64) / SECTOR_SIZE + 1;
   sector_count += entry_table_length;
+
+  // root directory
+  entries[0].entry_type = MYFS_DIR;
+  strcpy(entries[0].entry_name, "root");
+
   int i;
-  for(i = 0; i < entry_num; i++)
+  for(i = 1; i < entry_num; i++)
   {
-    strcpy(entrys[i].entry_name, argv[i*2+2]);
-    entrys[i].data_sector = sector_count;
+    strcpy(entries[i].entry_name, argv[i*2+2]);
+    entries[i].data_sector = sector_count;
     FILE *stream = fopen(argv[i*2+1], "r");
     if(stream == 0)
     {
@@ -33,29 +45,32 @@ int main(int argc, char **argv)
       return 1;
     }
     fseek(stream, 0, SEEK_END);
-    entrys[i].data_count = ftell(stream) / SECTOR_SIZE + 1;
-    entrys[i].entry_type = FS_FILE;
-    sector_count += entrys[i].data_count;
+    entries[i].data_size = ftell(stream);
+    entries[i].data_count = ftell(stream) / SECTOR_SIZE + 1;
+    entries[i].entry_type = MYFS_FILE;
+    sector_count += entries[i].data_count;
     printf("writing file %s->%s at sector: %ld count: %ld\n", 
-        argv[i*2+1], argv[i*2+2], entrys[i].data_sector, entrys[i].data_count);
+        argv[i*2+1], argv[i*2+2], entries[i].data_sector, entries[i].data_count);
     fclose(stream);
   }
 
   FILE *wstream = fopen("./kernel.img", "w");
   unsigned char *data = (unsigned char *)malloc(sector_count * SECTOR_SIZE);
 
-  // write entrys
-  fwrite(entrys, sizeof(struct myfs_entry), 64, wstream);
+  // write header
+  fwrite(&header, sizeof(struct myfs_struct), 1, wstream);
+  // write entries
+  fwrite(entries, sizeof(struct myfs_entry), 64, wstream);
 
   for(i = 0; i < entry_num; i++)
   {
     FILE *stream = fopen(argv[i*2+1], "r");
-    unsigned char *buf = (unsigned char *)malloc(entrys[i].data_count * SECTOR_SIZE);
-    fread(buf, 1, entrys[i].data_count * SECTOR_SIZE, stream);
+    unsigned char *buf = (unsigned char *)malloc(entries[i].data_count * SECTOR_SIZE);
+    fread(buf, 1, entries[i].data_count * SECTOR_SIZE, stream);
     // seek to data start sector
-    fseek(wstream, entrys[i].data_sector * SECTOR_SIZE, SEEK_SET);
+    fseek(wstream, entries[i].data_sector * SECTOR_SIZE, SEEK_SET);
     fseek(wstream, -(10 * SECTOR_SIZE), SEEK_CUR);
-    fwrite(buf, 1, entrys[i].data_count * SECTOR_SIZE, wstream);
+    fwrite(buf, 1, entries[i].data_count * SECTOR_SIZE, wstream);
     fclose(stream);
     free(buf);
   }
