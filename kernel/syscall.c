7040ca27a181b7e16713fa9b94b4df6f63b989c1
soildfaker <enbinli@outlook.com>
2018-01-16 22:24:32 +0800

    fix bug: print systemcall called kernel function

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -1,13 +1,14 @@
 #include "syscall.h"
 #include "debug.h"
 #include "drivers/display.h"
+#include "mm.h"
+#include "page.h"
 #include "tools.h"
 #include "task.h"
 #include "init.h"
 
 typedef int (*sysc_func) (struct trap_frame *r);
 
-extern void int_ret_stub(void);
 static void syscall_handler(struct trap_frame *frame);
 
 static sysc_func syscalls[SYSCALL_NUM];
@@ -19,16 +20,17 @@ int fork()
   return a;
 }
 
-int printf(const char *str, ...)
+int print_hex(u32 hex)
 {
   int a;
-  va_list args;
-	static char buff[1024];
-  va_start(args, str);
-	a = vsprintf(buff, str, args);
-	va_end(args);
-	buff[a] = '\0';
-  asm volatile("int $0x80" : "=a" (a) : "0" (0), "b"(buff));
+  asm volatile("int $0x80" : "=a" (a) : "0" (2), "b"(hex));
+  return a;
+}
+
+int print_str(const char *str)
+{
+  int a;
+  asm volatile("int $0x80" : "=a" (a) : "0" (0), "b"(str));
   return a;
 }
 
@@ -39,15 +41,22 @@ static int sys_fork(struct trap_frame *frame)
   new_task->frame = (struct trap_frame *)(new_task->context->esp);
   *new_task->frame = *frame;
   new_task->frame->eax = 0;
+  new_task->frame->esp = (u32)new_task->user_stack + USER_STACK_SIZE;
   new_task->context->eip = (u32)int_ret_stub;
   new_task->state = RUNNABLE;
   return new_task->pid;
 }
 
+static int sys_print_hex(struct trap_frame *frame)
+{
+  u32 hex = (u32)(frame->ebx);
+  display_print_hex(hex);
+  return 0;
+}
+
 static int sys_print(struct trap_frame *frame)
 {
   char *str = (char *)(frame->ebx);
-
 	display_print(str);
   return 0;
 }
@@ -63,6 +72,7 @@ void init_syscall()
   register_interrupt_handler (0x80, &syscall_handler);
   syscalls[0] = &sys_print;
   syscalls[1] = &sys_fork;
+  syscalls[2] = &sys_print_hex;
 }
 
 void syscall_handler(struct trap_frame *frame)
